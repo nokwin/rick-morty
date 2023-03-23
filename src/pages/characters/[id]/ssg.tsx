a9f83124c9bb7798136ef5335ae79ed48791b071
Dmytro Batarin <dmytro@batarin.dev>
2023-03-23 15:53:22 +0200

    SEO

--- src/pages/characters/[id]/ssg.tsx
+++ src/pages/characters/[id]/ssg.tsx
@@ -3,6 +3,7 @@ import { FC } from 'react';
 import Image from 'next/image';
 import { GetInternalCharacterDto } from '@app/api/dto/GetInternalCharacterDto';
 import { internalApi } from '@app/api/fetcher';
+import Head from 'next/head';
 
 interface CharacterSsgProps {
   data: GetInternalCharacterDto;
@@ -10,17 +11,54 @@ interface CharacterSsgProps {
 
 const CharacterSsr: FC<CharacterSsgProps> = ({ data }) => {
   return (
-    <div className="flex justify-center items-center h-screen gap-4">
-      <div>
-        <p>Name: {data.name}</p>
-        <p>Status: {data.status}</p>
-        <p>Species: {data.species}</p>
-        <p>Gender: {data.gender}</p>
-        <p>Origin: {data.origin}</p>
-        <p>Location: {data.location}</p>
+    <>
+      <Head>
+        <title>{`${data.name} | Rick & Morty DB`}</title>
+        <meta
+          name="description"
+          content={`All information about character ${data.name} in Rick & Morty cartoon`}
+        />
+
+        <meta
+          property="og:url"
+          content="https://witty-eagles-knock-188-24-153-202.loca.lt/characters/1"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={`${data.name} | Rick & Morty DB`} />
+        <meta
+          property="og:description"
+          content={`All information about character ${data.name} in Rick & Morty cartoon`}
+        />
+        <meta property="og:image" content={data.image} />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:domain"
+          content="witty-eagles-knock-188-24-153-202.loca.lt"
+        />
+        <meta
+          property="twitter:url"
+          content="https://witty-eagles-knock-188-24-153-202.loca.lt/characters/1"
+        />
+        <meta name="twitter:title" content={`${data.name} | Rick & Morty DB`} />
+        <meta
+          name="twitter:description"
+          content={`All information about character ${data.name} in Rick & Morty cartoon`}
+        />
+        <meta name="twitter:image" content={data.image} />
+      </Head>
+      <div className="flex justify-center items-center h-screen gap-4">
+        <div>
+          <p>Name: {data.name}</p>
+          <p>Status: {data.status}</p>
+          <p>Species: {data.species}</p>
+          <p>Gender: {data.gender}</p>
+          <p>Origin: {data.origin}</p>
+          <p>Location: {data.location}</p>
+        </div>
+        <Image src={data.image} alt={data.name} width="144" height="144" />
       </div>
-      <Image src={data.image} alt={data.name} width="144" height="144" />
-    </div>
+    </>
   );
 };
 
